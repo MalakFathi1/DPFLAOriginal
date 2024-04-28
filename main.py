73058fe226988bb3eb8ca0bb2d3bcd25b83a5986
Malak Fathi <malak.fathi368@gmail.com>
2024-04-28 16:59:42 +0300

    Kmeans clustering and inludes client dropout

---
+++ main.py
@@ -0,0 +1,29 @@
+from server import run_exp
+from federated_learning.my_dict import replace_1_with_7, replace_dog_with_cat
+from federated_learning.my_dict import run_mnist, run_cifar10
+from federated_learning.my_dict import fed_avg, simple_median, trimmed_mean, multi_krum, fools_gold, DPFLA
+
+if __name__ == '__main__':
+    NUM_WORKERS = 20
+    FRAC_WORKERS = 1
+    ATTACK_TYPE1 = "label_flipping"
+    ATTACK_TYPE2 = "label_flipping"
+    #ATTACK_TYPE = "backdoor"
+    GLOBAL_ROUND = 100
+    LOCAL_EPOCH = 1
+    UNTARGET = False
+
+    REPLACE_METHOD = replace_1_with_7()
+    RULE = DPFLA()
+    DATASET = run_mnist()
+
+    # REPLACE_METHOD = replace_dog_with_cat()
+    # RULE = DPFLA()
+    # DATASET = run_cifar10()
+
+    MALICIOUS_RATE = [0,0.1,0.2,0.3,0.4]  # 0, 0.1, 0.2, 0.3, 0.4, 0.5
+    MALICIOUS_BEHAVIOR_RATE = 1
+
+    for rate in MALICIOUS_RATE:
+        run_exp(NUM_WORKERS, FRAC_WORKERS, ATTACK_TYPE1,ATTACK_TYPE2, RULE, REPLACE_METHOD, DATASET, rate,
+                MALICIOUS_BEHAVIOR_RATE, GLOBAL_ROUND, LOCAL_EPOCH, UNTARGET)
